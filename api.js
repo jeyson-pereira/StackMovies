b1fe23b010d966b8bd283028f107a8049af8f93f
jeyson-pereira <jeyson.pereira@uniminuto.edu.co>
2021-08-10 18:35:27 -0500

    Refactor Fetch catch with toast

--- api.js
+++ api.js
@@ -1,25 +1,44 @@
 import { apiURL } from '@env';
+import {ToastAndroid} from 'react-native';
 
 //Get Movies Listed on Cinemas
 export const getMovies = async () => {
-    const movies = await fetch(`${apiURL}movies`)
-        .then(response => response.json());
+    let movies = null;
+    await fetch(`${apiURL}movies`)
+        .then(response => movies = response.json())
+        .catch((error) => {
+            if(error == 'TypeError: Network request failed'){
+                ToastAndroid.show('Error de conexión!', ToastAndroid.LONG);
+            }
+           });
 
     return movies;
 }
 
 //Get Info Movie Selected
 export const getMovieInfo = async (movie_id) => {
-    const movieInfo = await fetch(`${apiURL}movies/${movie_id}`)
-        .then(response => response.json());
+    let movieInfo = null;
+    await fetch(`${apiURL}movies/${movie_id}`)
+        .then(response => movieInfo = response.json())
+        .catch((error) => {
+            if(error == 'TypeError: Network request failed'){
+                ToastAndroid.show('Error de conexión!', ToastAndroid.LONG);
+            }
+           });
 
     return movieInfo;
 }
 
 //Get Cinemas Info and Movie Schedules 
 export const getCinemas = async (city, movie_inCity,) => {
-    const cinemas = await fetch(`${apiURL}movies/schedule/${city}/${movie_inCity}`)
-        .then(response => response.json());
+    let cinemas = null
+    await fetch(`${apiURL}movies/schedule/${city}/${movie_inCity}`)
+        .then(response => cinemas = response.json())
+        .catch((error) => {
+            if(error == 'TypeError: Network request failed'){
+                ToastAndroid.show('Error de conexión!', ToastAndroid.LONG);
+            }
+           });
 
     return cinemas;
 }
