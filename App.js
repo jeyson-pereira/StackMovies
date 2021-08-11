be97fbcb780b0e9627ee4bbf55ae72b1c7897630
jeyson-pereira <jeyson.pereira@uniminuto.edu.co>
2021-08-11 04:40:19 -0500

    Fix load cache

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Colors from './constants/Colors';
-
+import { LinearGradient } from 'expo-linear-gradient';
 import useCacheResources from './hooks/useCacheResources';
 import Navigation from './navigation/Navigation';
 
@@ -11,7 +11,14 @@ export default function App() {
   const isLoadingComplete = useCacheResources();
 
   if (!isLoadingComplete) {
-    return (null);
+    return (
+      <SafeAreaProvider>
+        <LinearGradient
+            colors={[Colors.bg, Colors.bgEnd]}
+            style={{ flex: 1 }}>
+        </LinearGradient>
+      </SafeAreaProvider>
+    );
   }
 
   return (
